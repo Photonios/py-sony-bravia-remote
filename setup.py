8e9d1e2b91bac84c98862f6542cd728c99ea2216
Swen Kooij <swen@sectorlabs.ro>
2017-07-02 00:32:21 +0300

    Change name to py-sony-bravia-remote

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 setup(
-    name='sony-bravia-remote',
+    name='py-sony-bravia-remote',
     version='1.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='Library for remotely interfacing with Sony Bravia TV\'s.',
     long_description=README,
-    url='https://github.com/Photonios/sony-bravia-remote',
+    url='https://github.com/Photonios/py-sony-bravia-remote',
     author='Swen Kooij',
     author_email='swenkooij@gmail.com',
     keywords=['sony', 'bravia', 'remote'],
